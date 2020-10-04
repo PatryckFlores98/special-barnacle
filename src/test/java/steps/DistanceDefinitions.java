685944848081b42bab1e0030b33bd2e84ce36c9b
Joao Davila <joao.davila@softmovel.com.br>
2020-10-04 16:11:40 -0300

    Cenario de teste e integração com o Cucumber

---
+++ src/test/java/steps/DistanceDefinitions.java
@@ -0,0 +1,23 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DistanceDefinitions {
+    @Given("I have kilometers to be calculated")
+    public void i_have_kilometers_to_be_calculated() throws  Exception {
+        throw new PendingException();
+    }
+
+    @When("I add kilometers travelled")
+    public void i_add_kilometers_travelled() throws Exception {
+        throw new PendingException();
+    }
+
+    @Then("I want to know how much time it takes")
+    public void i_want_to_know_how_much_time_it_takes() throws Exception {
+        throw new PendingException();
+    }
+}
---
+++ src/test/resource/distance.feature
@@ -0,0 +1,6 @@
+Feature: Distance feature
+
+  Scenario: Calculating the time traveled giving kilometers
+    Given I have kilometers to be calculated
+    When I add kilometers travelled
+    Then I want to know how much time it takes
