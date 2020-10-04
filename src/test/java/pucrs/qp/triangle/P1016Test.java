49a1801e49c25348de14a387d2bf933478c087aa
PatryckFlores98 <patryck.flores@acad.pucrs.br>
2020-10-03 22:15:52 -0300

    add classe que multiplica a distancia e a classe de teste

---
+++ src/test/java/pucrs/qp/triangle/P1016Test.java
@@ -0,0 +1,18 @@
+package pucrs.qp.triangle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class P1016Test {
+
+	@Test
+	public void test() {
+		
+		fail("Not yet implemented");
+		int actual = P1016.multiplica(3);
+		int expected=6;
+		assertEquals(expected, actual);
+	}
+
+}
